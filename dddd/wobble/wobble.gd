04a556435729ef1d464c8c92e0a7a5f5c31012a1
Eric Taylor <erictaylor4954@gmail.com>
2026-04-25 14:09:01 -0700

    wobble wobble

---
+++ dddd/wobble/wobble.gd
@@ -0,0 +1,30 @@
+extends Node3D
+
+@export var wobble_duration: float = 0.5
+
+var material: ShaderMaterial
+var wobble_time: float = 0.0
+var is_wobbling: bool = false
+
+@onready var mesh = $MeshInstance3D
+
+func _ready() -> void:
+	material = mesh.get_active_material(0) as ShaderMaterial
+
+func trigger_wobble() -> void:
+	wobble_time = 0.0
+	is_wobbling = true
+
+func _process(delta: float) -> void:
+	
+	if not is_wobbling:
+		return
+	
+	wobble_time += delta
+	var progress: float = wobble_time / wobble_duration
+	
+	if progress >= 1.0:
+		progress = 1.0
+		is_wobbling = false
+	
+	material.set_shader_parameter("wobble_amount", progress)
