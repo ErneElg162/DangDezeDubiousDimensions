104564d7b75f9f0c8e6d334c578a3656474d448e
Ernesto Elguezabal <ernestoelg50@gmail.com>
2026-04-25 16:23:12 -0700

    removed gun collision

--- dddd/Character/character_body_3d.gd
+++ dddd/Character/character_body_3d.gd
@@ -65,4 +65,6 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
+	print(velocity)
+
 	move_and_slide()
