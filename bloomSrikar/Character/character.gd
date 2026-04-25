ee65738e1cc77c4faa56912c734e54372b950037
SirPopGhost <47957443+SirPopGhost@users.noreply.github.com>
2026-04-25 12:00:34 -0700

    Fixed camera and player movement

--- bloomSrikar/Character/character.gd
+++ bloomSrikar/Character/character.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 @export var look_sensitivity: float = 0.005
 
 @onready var head = $Head
-@onready var camera: Camera3D = $Head/Camera3D
+@onready var camera: Camera3D = $Camera3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
@@ -13,7 +13,7 @@ func _ready():
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-		head.rotate_y(-event.relative.x * look_sensitivity)
+		rotate_y(-event.relative.x * look_sensitivity)
 		camera.rotate_x(-event.relative.y + look_sensitivity)
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-80), deg_to_rad(80))
 		
@@ -38,14 +38,12 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "back")
 	
-	var direction := (camera.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	print(velocity)
 
 	move_and_slide()
